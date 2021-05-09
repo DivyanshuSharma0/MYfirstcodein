7b4fa31bb6912a7db56ce03da4cfd55c3d8b019c
DivyanshuSharma0 <83811672+DivyanshuSharma0@users.noreply.github.com>
2021-05-09 13:56:26 +0530

    Initaial code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 1; i < 100;i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("The no. divisible by 3 are " + i);
+                }
+            }
+        }
+    }
+}
