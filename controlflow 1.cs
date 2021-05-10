06a8793711a7d5fb21920dc44c3991e0294d71fd
DivyanshuSharma0 <83811672+DivyanshuSharma0@users.noreply.github.com>
2021-05-10 14:45:39 +0530

    Add files via upload

---
+++ controlflow 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, c = 0;
+            for (i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    c++;
+                }
+            }
+            Console.WriteLine("the count of no which is divisible by 3 till 100 is "+ c);
+            Console.ReadLine();
+        }
+    }
+}
