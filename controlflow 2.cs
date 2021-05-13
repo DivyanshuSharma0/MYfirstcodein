4a56e3118cd3f719fd037dd17238b57a28c78b01
DivyanshuSharma0 <83811672+DivyanshuSharma0@users.noreply.github.com>
2021-05-13 14:50:48 +0530

    initial commit

---
+++ controlflow 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, num, fact = 1;
+            Console.WriteLine("The no. which you want to factorize");
+            num = int.Parse(Console.ReadLine());
+            for (i = 1; i < num + 1; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("The factorial of " + num + " is " + fact);
+            Console.ReadLine();
+        }
+    }
+}
+
